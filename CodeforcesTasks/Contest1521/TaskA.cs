9da0b0446b323bfd30670122e08fbd2c38baf544
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-05-20 09:04:08 +0300

    Add 1521A

---
+++ CodeforcesTasks/Contest1521/TaskA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1521
+{
+    // https://codeforces.com/contest/1521/problem/A
+    public class TaskB
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var numbers = Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
+                var (a, b) = (numbers[0], numbers[1]);
+                Console.WriteLine(b == 1 ? "NO" : $"YES {a} {a * b} {a * (b + 1)}");
+            }
+
+        }
+    }
+}
