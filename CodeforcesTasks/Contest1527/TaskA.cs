deaede7c3f3dafb05394757fd4333fbb4b9e6932
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-05-21 11:38:25 +0300

    Add 1527 A

---
+++ CodeforcesTasks/Contest1527/TaskA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1527
+{
+    public class TaskA
+    {
+        static void Main(string[] args)
+        {
+            var testNumber = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < testNumber; i++)
+            {
+                var number = long.Parse(Console.ReadLine()!);
+
+                long result = 0;
+                
+                while (number > 1)
+                    result |= (number >>= 1);
+                
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
