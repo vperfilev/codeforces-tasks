e04c75dfa16e3c38a39c1b90d571460f5d78e91a
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-05-25 11:30:44 +0300

    Add 1520B

---
+++ CodeforcesTasks/Contest1520/TaskB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+namespace CodeforcesTasks.Contest1520
+{
+    // https://codeforces.com/contest/1520/problem/B
+    public class TaskB
+    {
+        static void Main(string[] args)
+        {
+            var testCount = int.Parse(Console.ReadLine()!);
+            for (var testNo = 0; testNo < testCount; testNo++)
+            {
+                var result = FindSolution(Console.ReadLine());
+                Console.WriteLine(result);
+            }
+        }
+        
+        public static long FindSolution(string number)
+        {
+            var addPoint = number[0];
+            foreach(var digit in number)
+            {
+                if (addPoint == digit) continue;
+                if (digit < addPoint) addPoint--;
+                break;
+            }
+ 
+            return (number.Length - 1) * 9 + int.Parse(addPoint.ToString());
+        }
+    }
+}
--- TaskTests/Contest1520Tests.cs
+++ TaskTests/Contest1520Tests.cs
@@ -14,5 +14,19 @@ namespace TaskTests
             Assert.False(TaskA.IsSuspicious("Z"));
             Assert.False(TaskA.IsSuspicious("AB"));
         }
+
+        [Fact]
+        public void TaskBTest()
+        {   
+            Assert.Equal(1, TaskB.FindSolution("1"));
+            Assert.Equal(2, TaskB.FindSolution("2"));
+            Assert.Equal(3, TaskB.FindSolution("3"));
+            Assert.Equal(4, TaskB.FindSolution("4"));
+            Assert.Equal(5, TaskB.FindSolution("5"));
+            Assert.Equal(10, TaskB.FindSolution("20"));
+            Assert.Equal(17, TaskB.FindSolution("95"));
+            Assert.Equal(18, TaskB.FindSolution("100"));
+            Assert.Equal(19, TaskB.FindSolution("120"));
+        }
     }
 }
