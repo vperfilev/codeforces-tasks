2b426c6ce8e8a329544542abecd695b9f4229857
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-06-02 12:41:30 +0300

    Add 1529A

---
+++ CodeforcesTasks/Contest1529/TaskA.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1529
+{
+    public class TaskA
+    {
+        static void Main(string[] args)
+        {
+            var testCount = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < testCount; i++)
+            {
+                Console.ReadLine();
+                var array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+                var minValue = int.MaxValue;
+                var minValueCount = 0;
+                foreach (var value in array)
+                {
+                    if (value > minValue) continue;
+                    if (value == minValue)
+                    {
+                        minValueCount++;
+                    }
+                    else
+                    {
+                        minValueCount = 1;
+                        minValue = value;
+                    }
+                }
+
+                Console.WriteLine(array.Length - minValueCount);
+            }
+        }
+    }
+}
