9bee458945c2a880346d118dd54e3f6cced2ab3b
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-06-08 09:24:39 +0300

    Add 1526B

---
+++ CodeforcesTasks/Contest1526/TaskB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1526
+{
+    // https://codeforces.com/contest/1526/problem/B
+    public class TaskB
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < count; i++)
+            {
+                var number = int.Parse(Console.ReadLine()!);
+                Console.WriteLine(Solve(number) ? "YES" : "NO");
+            }
+        }
+        
+        public static bool Solve(int number)
+        {
+            if(number > 1100) return true;
+	
+            while (number >= 11)
+            {
+                if (number % 11 == 0) return true;
+                number -= 111;
+            }
+            return number == 0 ? true : false;
+        }
+    }
+}
--- TaskTests/Contest1526Tests.cs
+++ TaskTests/Contest1526Tests.cs
@@ -24,5 +24,14 @@ namespace TaskTests
             var data = Array.ConvertAll("1 1 2 0 1 -1 -1 -4 1 3 -4 3 0 1 -1 3 0 1 0 0 1 -1 -1 -2 -1 3 1 0 -1 3 -2 -4 -2 -3 -2 -3 2 -3 -1 0 -1 -3 1 -1 3 -1 2 1 2 -3 2 0 2 2 1 1 0 -1 -4 0 2 -1 -4 1 3 2 -2 0 3 -2 -4 2 -4 1 2 2 -2 1 2 -3 3 -4 1 -2 -4 0 -1 3 2 -4 3 -3 -2 -3 -3 -4 1 3 2 1 -4 1 -4 3 2 0 -1 3 1 -4 -3 -1 -3 -3 3 2 -3 0 2 2 -4 -4 -1 2 1 -2 3 -2 -1 -3 3 1 -2 -2 1 2 0 1 1 2 -3 2 -3 0 -1 1 2 -3 -1 2 1 1 -2 -2 -1 -4 -3 0 -3 -3 -2 1 0 -1 0 -3 2 -1 -2 -4 -1 -1 1 2 1 -2 2 -1 -3 -2 1 -1 -4 -3 1 2 3 -4 1 -2 3 3 -4 0 1 3 2 -1 -1 2 -3 1 1 3 -3".Split(), int.Parse);
             Assert.Equal(187, TaskC2.Solve(data));
         }
+
+        [Fact]
+        public void TaskBTest()
+        {
+            Assert.True(TaskB.Solve(33));
+            Assert.True(TaskB.Solve(144));
+            Assert.True(TaskB.Solve(1440));
+            Assert.False(TaskB.Solve(69));
+        }
     }
 }
