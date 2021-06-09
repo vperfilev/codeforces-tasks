1c5f53ef2967f765126fa7dfb3097ad25177c2ea
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-06-09 10:33:59 +0300

    Add 1535A

---
+++ CodeforcesTasks/Contest1535/TaskA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1535
+{
+    
+    // https://codeforces.com/contest/1535/problem/A
+    public class TaskA
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < count; i++)
+            {
+                var input = Read();
+                var isFair  = Solve(input[0], input[1], input[2], input[3]);
+                Console.WriteLine(isFair ? "YES" : "NO");
+            }
+        }
+        
+        public static bool Solve(int a, int b, int c, int d) 
+        {
+            (a,b) = a<b? (a,b) : (b,a);
+            (c,d) = c<d? (c,d) : (d,c);
+            return b > c && d > a;
+        }
+    }
+}
---
+++ TaskTests/Contest1535Tests.cs
@@ -0,0 +1,17 @@
+﻿using CodeforcesTasks.Contest1535;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Contest1535Tests
+    {
+        [Fact]
+        public void TaskATest()
+        {
+            Assert.True(TaskA.Solve(3, 7, 9, 5));
+            Assert.False(TaskA.Solve(4, 5, 6, 9));
+            Assert.True(TaskA.Solve(5, 3, 8, 1));
+            Assert.False(TaskA.Solve(6, 5, 3, 2));
+        }
+    }
+}
