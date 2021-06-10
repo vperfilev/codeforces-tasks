14b752426fef8946b6fd5ab2907309aca7228151
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-06-10 09:35:32 +0300

    Add 1526 Bv2

---
+++ CodeforcesTasks/Contest1526/TaskBv2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1526
+{
+    // https://codeforces.com/contest/1526/problem/B
+    public class TaskBv2
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var number = int.Parse(Console.ReadLine()!);
+                Console.WriteLine(number % 11 <= number / 111 ? "YES" : "NO");
+            }
+        }
+    }
+}
