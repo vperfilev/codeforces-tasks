eb39a9127829704bfa2257b7779bc3b0ace93e38
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-06-11 16:19:36 +0300

    Add 1535B

---
+++ CodeforcesTasks/Contest1535/TaskB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1535
+{
+    
+    // https://codeforces.com/contest/1535/problem/B
+    public class TaskB
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                Console.ReadLine();
+                Console.WriteLine(Solve(Read()));
+            }
+        }
+        
+        public static int Solve(int[] values)
+        {
+            var size = values.Length;
+            var odd = values.Where(v => v%2 == 1).ToArray();
+            var oddCount = odd.Length;
+            var evenCount = size - oddCount;
+            var result = evenCount * (evenCount - 1) / 2 + evenCount * oddCount;
+	
+            for(var i = 0; i < oddCount; i++)
+            {
+                for(var j = i + 1; j < oddCount; j++)
+                {
+                    if (FindGcd(odd[i], odd[j]) > 1) result++;
+                }
+            }
+            return result;
+        }
+
+        private static int FindGcd(int a, int b) 
+        {
+            (a,b) = a<=b ? (a,b) : (b,a);
+            while (true)
+            {
+                var remainder = a % b;
+                if (remainder == 0) break;
+                (b, a) = b < remainder ? (b, remainder) : (remainder, b);
+            }
+            return b;
+        }
+    }
+}
--- TaskTests/Contest1535Tests.cs
+++ TaskTests/Contest1535Tests.cs
@@ -13,5 +13,13 @@ namespace TaskTests
             Assert.True(TaskA.Solve(5, 3, 8, 1));
             Assert.False(TaskA.Solve(6, 5, 3, 2));
         }
+        
+        [Fact]
+        public void TaskBTest()
+        {
+            Assert.Equal(4, TaskB.Solve(new [] {3, 6, 5, 3}));
+            Assert.Equal(0, TaskB.Solve(new [] {1, 7}));
+            Assert.Equal(9, TaskB.Solve(new [] {1, 4, 2, 4, 1}));
+        }
     }
 }
