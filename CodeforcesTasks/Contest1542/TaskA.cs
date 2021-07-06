a4b51d279597b9896aa742a6123fb53b4b91d3d5
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-07-06 12:56:57 +0300

    Add 1542A

---
+++ CodeforcesTasks/Contest1542/TaskA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1542
+{
+    public class TaskA
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                Console.ReadLine();
+                Console.WriteLine(Solve(Read()) ? "YES" : "NO");
+            }
+        }
+
+        public static bool Solve(int[] values) => 
+            values.Length == 2 * values.Count(value => value % 2 == 0);
+    }
+}
---
+++ TaskTests/Contest1542Tests.cs
@@ -0,0 +1,18 @@
+﻿using CodeforcesTasks.Contest1542;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Contest1542Tests
+    {
+        [Fact]
+        public void TaskATest()
+        {
+            Assert.True(TaskA.Solve(new [] {2, 3, 4, 5}));
+            Assert.False(TaskA.Solve(new [] {2, 3, 4, 5, 5, 5}));
+            Assert.False(TaskA.Solve(new [] {2, 4}));
+            Assert.True(TaskA.Solve(new [] {2, 3}));
+            Assert.False(TaskA.Solve(new [] {1, 5, 3, 2, 6, 7, 3, 4}));
+        }
+    }
+}
