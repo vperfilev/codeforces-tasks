ca06481f922384cc7e82d54e2de7605f94413d3d
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-07-08 13:15:30 +0300

    Add 1541A

---
+++ CodeforcesTasks/Contest1541/TaskA.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeforcesTasks.Contest1541
+{
+    // https://codeforces.com/contest/1541/problem/A
+    public class TaskA
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                Console.WriteLine(string.Join(" ", Solve(n)));
+            }
+        }
+        
+        public static IEnumerable<int> Solve(int n)
+        {
+            var number = 0;
+            var remainder = 1;
+            if (n % 2 == 1)
+            {
+                yield return 3;
+                yield return 1;
+                yield return 2;
+                number = 3;
+                remainder  = 0;
+            }
+
+            while (number++ < n)
+            {
+                if (number % 2 == remainder ) yield return number + 1;
+                else yield return number - 1;
+            }
+        }
+    }
+}
--- TaskTests/Contest1542Tests.cs
+++ TaskTests/Contest1542Tests.cs
@@ -14,5 +14,15 @@ namespace TaskTests
             Assert.True(TaskA.Solve(new [] {2, 3}));
             Assert.False(TaskA.Solve(new [] {1, 5, 3, 2, 6, 7, 3, 4}));
         }
+
+        [Fact]
+        public void TaskBTest()
+        {
+            Assert.True(TaskB.Solve(24, 3, 5));
+            Assert.False(TaskB.Solve(10, 3, 6));
+            Assert.True(TaskB.Solve(2345, 1, 4));
+            Assert.False(TaskB.Solve(19260817, 394, 485));
+            Assert.True(TaskB.Solve(19260817, 233, 264));
+        }
     }
 }
