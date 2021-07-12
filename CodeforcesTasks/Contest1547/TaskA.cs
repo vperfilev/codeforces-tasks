7b85f297328cae7d8da560f366924624cd1bb807
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-07-12 11:51:56 +0300

    Add 1547A

---
+++ CodeforcesTasks/Contest1547/TaskA.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1547
+{
+    // https://codeforces.com/contest/1547/problem/A
+    public class TaskA
+    {
+        private static (int, int) Read2()
+        {
+            var values = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            return (values[0], values[1]);
+        }
+
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                Console.ReadLine();
+                var a = Read2();
+                var b = Read2();
+                var f = Read2();
+                Console.WriteLine(Solve(a, b, f));
+            }
+        }
+
+        public static int Solve((int x, int y) a, (int x, int y) b, (int x, int y) f)
+        {
+            var (minX, maxX) = a.x < b.x ? (a.x, b.x) : (b.x, a.x);
+            var (minY, maxY) = a.y < b.y ? (a.y, b.y) : (b.y, a.y);
+
+            var result = maxX - minX + maxY - minY;
+            
+            if (minX == f.x && maxX == f.x && minY < f.y && f.y < maxY)
+            {
+                return result + 2;
+            } 
+            
+            if (minY == f.y && maxY == f.y && minX < f.x && f.x < maxX)
+            {
+                return result + 2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TaskTests/Content1547Tests.cs
@@ -0,0 +1,20 @@
+﻿using CodeforcesTasks.Contest1547;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Content1547Tests
+    {
+        [Fact]
+        public void TaskATest()
+        {
+            Assert.Equal(4, TaskA.Solve((1, 1), (3, 3), (2, 2)));
+            Assert.Equal(6, TaskA.Solve((2, 5), (2, 1), (2, 3)));
+            Assert.Equal(41, TaskA.Solve((1000, 42), (1000, 1), (1000, 1000)));
+            Assert.Equal(4, TaskA.Solve((1, 10), (3, 10), (2, 10)));
+            Assert.Equal(4, TaskA.Solve((3, 8), (7, 8), (3, 7)));
+            Assert.Equal(2, TaskA.Solve((2, 1), (4, 1), (1, 1)));
+            Assert.Equal(334, TaskA.Solve((1, 344), (1, 10), (1, 1)));              
+        }
+    }
+}
