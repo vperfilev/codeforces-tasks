ad7311422229f188dced56534793d7739aae8789
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-07-14 09:24:47 +0300

    Add 1547B

---
+++ CodeforcesTasks/Contest1547/TaskB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1547
+{
+    public class TaskB
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var line = Console.ReadLine();
+                Console.WriteLine(Solve(line) ? "YES" : "NO");
+            }
+        }
+        
+        public static bool Solve(string input)
+        {
+            var nextLetter = 'a';
+            
+            var start = input.IndexOf(nextLetter);
+            if (start == -1) return false;
+            
+            var downIndex = start - 1;
+            var upIndex = start + 1;
+            while (nextLetter < 'z' && (downIndex >= 0 || upIndex < input.Length))
+            {
+                nextLetter++;
+                
+                if (downIndex >= 0 && input[downIndex] == nextLetter)
+                    downIndex--;
+                else if (upIndex < input.Length && input[upIndex] == nextLetter)
+                    upIndex++;
+                else
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- TaskTests/Content1547Tests.cs
+++ TaskTests/Content1547Tests.cs
@@ -14,7 +14,25 @@ namespace TaskTests
             Assert.Equal(4, TaskA.Solve((1, 10), (3, 10), (2, 10)));
             Assert.Equal(4, TaskA.Solve((3, 8), (7, 8), (3, 7)));
             Assert.Equal(2, TaskA.Solve((2, 1), (4, 1), (1, 1)));
-            Assert.Equal(334, TaskA.Solve((1, 344), (1, 10), (1, 1)));              
+            Assert.Equal(334, TaskA.Solve((1, 344), (1, 10), (1, 1)));
+
         }
+
+        [Fact]
+        public void TaskBTest()
+        {
+            Assert.True(TaskB.Solve("a"));
+            Assert.True(TaskB.Solve("ba"));
+            Assert.True(TaskB.Solve("ab"));
+            Assert.True(TaskB.Solve("bac"));
+            Assert.True(TaskB.Solve("ihfcbadeg"));
+            Assert.False(TaskB.Solve("z"));
+            Assert.False(TaskB.Solve("aa"));
+            Assert.False(TaskB.Solve("ca"));
+            Assert.False(TaskB.Solve("acb"));
+            Assert.False(TaskB.Solve("xyz"));
+            Assert.False(TaskB.Solve("ddcba"));
+        }
+
     }
 }
