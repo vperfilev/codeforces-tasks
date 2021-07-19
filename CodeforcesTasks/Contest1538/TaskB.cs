7c2c38ba82951874fcdb4ad4128d297fe216365f
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-07-19 13:51:53 +0300

    Add 1538B

---
+++ CodeforcesTasks/Contest1538/TaskB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1538
+{
+    // https://codeforces.com/contest/1538/problem/B
+    public class TaskB
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                Console.WriteLine(Solve(n, Read()));
+            }
+        }
+        
+        public static int Solve(int n, int[] candies)
+        {
+            if (n == 1) return 0;
+            
+            var total = candies[0];
+            var elementsAreEqual = true;
+            for (var i = 1; i < n; i++)
+            {
+                elementsAreEqual &= candies[i] == candies[i - 1];
+                total += candies[i];
+            }
+
+            if (elementsAreEqual) return 0;
+
+            if (total % n != 0) return -1;
+
+            var average = total / n;
+            var result = 0;
+            for (var i = 0; i < n; i++)
+            {
+                if (candies[i] > average) result++;
+            }
+            return result;
+        }
+    }
+}
--- TaskTests/Content1538Tests.cs
+++ TaskTests/Content1538Tests.cs
@@ -14,5 +14,15 @@ namespace TaskTests
             Assert.Equal(3, TaskA.Solve(4, new [] {3, 4, 2, 1}));
             Assert.Equal(2, TaskA.Solve(4, new [] {2, 3, 1, 4}));            
         }
+
+        [Fact]
+        public void TaskBTest()
+        {
+            Assert.Equal(2, TaskB.Solve(4, new [] {4, 5, 2, 5}));
+            Assert.Equal(1, TaskB.Solve(2, new [] {0, 4}));
+            Assert.Equal(-1, TaskB.Solve(5, new [] {10, 8, 5, 1, 4}));
+            Assert.Equal(0, TaskB.Solve(1, new [] {10000}));
+            Assert.Equal(0, TaskB.Solve(7, new [] {1, 1, 1, 1, 1, 1, 1}));
+        }
     }
 }
