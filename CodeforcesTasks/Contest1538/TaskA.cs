eda018ac1b10ba86322f017eb9a48d65e42f35a6
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-07-19 13:18:45 +0300

    Add 1538A

---
+++ CodeforcesTasks/Contest1538/TaskA.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1538
+{
+    // https://codeforces.com/contest/1538/problem/A
+    public class TaskA
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                Console.WriteLine(Solve(n, Read()));
+            }
+        }
+        
+        public static int Solve(int count, int[] rocks)
+        {
+            var min = (value: int.MaxValue, position: -1);
+            var max = (value: int.MinValue, position: -1);
+            
+            for (var i = 0; i < count; i++)
+            {
+                var rock = rocks[i];
+                if (rock > max.value) max = (value: rock, position: i);
+                if (rock < min.value) min = (value: rock, position: i);
+            }
+
+            var minLeft = min.position + 1;
+            var minRight = count - min.position;
+            var maxLeft = max.position + 1;
+            var maxRight = count - max.position;
+            
+            return new[]
+            {
+                Math.Max(maxLeft, minLeft), Math.Max(maxRight, minRight),
+                maxLeft + minRight, minLeft + maxRight
+            }.Min();
+        }
+    }
+}
---
+++ TaskTests/Content1538Tests.cs
@@ -0,0 +1,18 @@
+﻿using CodeforcesTasks.Contest1538;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Content1538Tests
+    {
+        [Fact]
+        public void TaskATest()
+        {
+            Assert.Equal(2, TaskA.Solve(5, new [] {1, 5, 4, 3, 2}));
+            Assert.Equal(4, TaskA.Solve(8, new [] {2, 1, 3, 4, 5, 6, 8, 7}));
+            Assert.Equal(5, TaskA.Solve(8, new [] {4, 2, 3, 1, 8, 6, 7, 5}));
+            Assert.Equal(3, TaskA.Solve(4, new [] {3, 4, 2, 1}));
+            Assert.Equal(2, TaskA.Solve(4, new [] {2, 3, 1, 4}));            
+        }
+    }
+}
