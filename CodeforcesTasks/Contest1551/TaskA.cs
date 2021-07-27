1a82222628fdf97427c6f28e03728cd9bc8b14e6
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-07-27 12:21:23 +0300

    Add 1551A

---
+++ CodeforcesTasks/Contest1551/TaskA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1551
+{
+    public class TaskA
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var (oneCount, twoCount) = Solve(int.Parse(Console.ReadLine()!));
+                Console.WriteLine($"{oneCount} {twoCount}");
+            }
+        }
+        public static (int oneCount, int twoCount) Solve(int amount)
+        {
+            var oneCount = (amount) / 3;
+            var twoCount = oneCount;
+            if (amount % 3 == 1) oneCount++;
+            else if (amount % 3 == 2) twoCount++;
+            return (oneCount, twoCount);
+        }
+    }
+}
---
+++ TaskTests/Contest1551Tests.cs
@@ -0,0 +1,25 @@
+﻿using CodeforcesTasks.Contest1551;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Contest1551Tests
+    {
+        [Fact]
+        public void TaskATest()
+        {
+            Assert.Equal((334, 333), TaskA.Solve(1000));
+            Assert.Equal((10, 10), TaskA.Solve(30));
+            Assert.Equal((1, 0), TaskA.Solve(1));
+            Assert.Equal((10, 11), TaskA.Solve(32));
+            Assert.Equal((333333334, 333333333), TaskA.Solve(1000000000));
+            Assert.Equal((1, 2), TaskA.Solve(5));
+            
+            
+            
+            
+            
+            
+        }
+    }
+}
