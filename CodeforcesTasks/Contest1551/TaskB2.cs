3d9aaa289accd6ebaa44eda3dba8369d6ad7c328
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-07-30 14:16:53 +0300

    Add 1551B2

---
+++ CodeforcesTasks/Contest1551/TaskB2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1551
+{
+    // https://codeforces.com/contest/1551/problem/B2
+    public class TaskB2
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var colorCount = Read()[1];
+                Console.WriteLine(Solve(Read(), colorCount));
+            }
+        }
+        
+        public static string Solve(int[] sequence, int colourCount)
+        {
+            var nodes = sequence.Select(value => new Node{Value = value, Color =  0}).ToArray();
+
+            var selectedNodes = nodes.GroupBy(x => x.Value)
+                .Select(group => group.Take(colourCount))
+                .SelectMany(x => x).ToList();
+            var nodeToSkip = selectedNodes.Count % colourCount;
+
+            var color = 0;
+            foreach (var node in selectedNodes.Skip(nodeToSkip)) node.Color = (color++ % colourCount) + 1;
+            
+            return string.Join(' ', nodes.Select(x => x.Color));
+        }
+
+        private class Node
+        {
+            public int Value { get; set; }
+            public int Color { get; set; }
+        }
+    }
+}
--- TaskTests/Contest1551Tests.cs
+++ TaskTests/Contest1551Tests.cs
@@ -1,4 +1,6 @@
-﻿using CodeforcesTasks.Contest1551;
+﻿using System.Diagnostics;
+using System.Linq;
+using CodeforcesTasks.Contest1551;
 using Xunit;
 
 namespace TaskTests
@@ -24,6 +26,24 @@ namespace TaskTests
             Assert.Equal(3, TaskB1.Solve("archive"));
             Assert.Equal(0, TaskB1.Solve("y"));
             Assert.Equal(1, TaskB1.Solve("xxxxxx"));
+        }
+
+        [Fact]
+        public void TaskB2Test()
+        {
+            Assert.Equal("0 1 3 1 2 0 3 2 1 2 3", TaskB2.Solve(new []{5, 3, 1, 1, 1, 1, 10, 3, 10, 10, 2}, 3));
+            Assert.Equal("1 3 1 2 0 3 2 1 2 3", TaskB2.Solve(new []{3, 1, 1, 1, 1, 10, 3, 10, 10, 2}, 3));
+            Assert.Equal("1 2 3 4", TaskB2.Solve(new []{1, 1, 1, 1}, 4));
+            Assert.Equal("1", TaskB2.Solve(new []{1}, 1));
+            Assert.Equal("1 1 1 0 1 1 1 1 0 0 0 1 0", TaskB2.Solve(new []{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9}, 1));
+            Assert.Equal("1 1 1 2 2 2 2 1 1 2 0 2 1", TaskB2.Solve(new []{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9}, 2));
+            Assert.Equal("0 2 1 3 2 2 1 2 3 1 1 3 3", TaskB2.Solve(new []{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9}, 3));
+            
+            var sw = Stopwatch.StartNew();
+            var result = TaskB2.Solve(Enumerable.Range(1, 200000).ToArray(), 200000);
+            var elapsedMilliseconds = sw.ElapsedMilliseconds;
+            Assert.True(elapsedMilliseconds < 2000);
+            Assert.Equal(string.Join(' ', Enumerable.Range(1, 200000).ToArray()), result);
         }
     }
 }
