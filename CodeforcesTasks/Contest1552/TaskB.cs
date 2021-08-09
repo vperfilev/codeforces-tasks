3b4edf6c36d2aa24f087e180ea3a79c37ec283be
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-08-09 10:08:41 +0300

    Add 1552B

---
+++ CodeforcesTasks/Contest1552/TaskB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1552
+{
+    // https://codeforces.com/contest/1552/problem/B
+    public class TaskB
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var setCount = int.Parse(Console.ReadLine()!);
+                var sets = new int[setCount][];
+                for (var j = 0; j < setCount; j++)
+                {
+                    sets[j] = Read();
+                }
+                Console.WriteLine(Solve(sets));
+            }
+        }
+        
+        public static int Solve(int[][] positions)
+        {
+            var greatestPos = 0;
+            var greatestValues = positions[0];
+            
+            for (var i = 1; i < positions.Length; i++)
+            {
+                if (!FirstIsGreater(positions[i], greatestValues)) continue;
+                greatestValues = positions[i];
+                greatestPos = i;
+            }
+
+            return positions.All(x => FirstIsGreater(greatestValues, x)) ? greatestPos + 1 : -1;
+        }
+
+        private static bool FirstIsGreater(int[] first, int[] second)
+        {
+            var firstWin = 0;
+            for (var i = 0; i < 5; i++)
+            {
+                if (first[i] <= second[i]) firstWin++;
+            }
+
+            return firstWin >= 3;
+        }
+    }
+}
--- TaskTests/Contest1552Tests.cs
+++ TaskTests/Contest1552Tests.cs
@@ -13,5 +13,15 @@ namespace TaskTests
             Assert.Equal(0, TaskA.Solve("aaaaa"));
             Assert.Equal(4, TaskA.Solve("dcba"));
         }
+
+        [Fact]
+        public void TaskBTest()
+        {
+            Assert.Equal(1, TaskB.Solve(new []{new []{50000, 1, 50000, 50000, 50000}}));            
+            Assert.Equal(-1, TaskB.Solve(new []{new []{10, 10, 20, 30, 30}, new []{20, 20, 30, 10, 10}, new []{30, 30, 10, 20, 20}}));            
+            Assert.Equal(1, TaskB.Solve(new []{new []{1, 1, 1, 1, 1}, new []{2, 2, 2, 2, 2}, new []{3, 3, 3, 3, 3}}));            
+            Assert.Equal(5, TaskB.Solve(new []{new []{9, 5, 3, 7, 1}, new []{7, 4, 1, 6, 8}, new []{5, 6, 7, 3, 2}, new []{6, 7, 8, 8, 6}, new []{4, 2, 2, 4, 5}, new []{8, 3, 6, 9, 4}}));            
+            Assert.Equal(1, TaskB.Solve(new []{new []{12268, 40495, 25519, 3323, 22033}, new []{36932, 25287, 21924, 33587, 40516}}));            
+        }
     }
 }
