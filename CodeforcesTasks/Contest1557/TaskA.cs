8663d13c6a8fa4b29d2ffd21096dcd5153b3a348
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-08-10 11:10:27 +0300

    Add 1557A

---
+++ CodeforcesTasks/Contest1557/TaskA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1557
+{
+    public class TaskA
+    {
+        static long[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                Console.ReadLine();
+                Console.WriteLine(Solve(Read()));
+            }
+        }
+        public static double Solve(long[] values)
+        {
+            var max = values.Max();
+            var sum = values.Sum();
+            return (sum - max) * 1.0 / (values.Length - 1) + max;
+        }
+    }
+}
---
+++ TaskTests/Contest1557Tests.cs
@@ -0,0 +1,26 @@
+﻿using CodeforcesTasks.Contest1557;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Contest1557Tests
+    {
+        [Fact]
+        public void TaskATest()
+        {
+            Assert.True(TaskA.Solve(new long[] { 3, 1, 2 }) - 4.5 < 0.0000001);
+            Assert.True(TaskA.Solve(new long[] { -7, -6, -6 }) + 12.5 < 0.0000001);
+            Assert.True(TaskA.Solve(new long[] { 2, 2, 2 })- 4 < 0.0000001);
+            Assert.True(TaskA.Solve(new long[] { 17, 3, 5, -3 })- 18.666666667 < 0.0000001);
+        }
+        /*
+3, 1, 2
+
+4.500000000
+-12.500000000
+4.000000000
+18.666666667
+
+         */
+    }
+}
