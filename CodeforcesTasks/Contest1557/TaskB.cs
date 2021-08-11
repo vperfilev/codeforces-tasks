fc36a7dbfd42a9642291779c28cea869c3108ada
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-08-11 11:16:02 +0300

    Add 1557B

---
+++ CodeforcesTasks/Contest1557/TaskB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1557
+{
+    public class TaskB
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var p = Read();
+                Console.WriteLine(Solve(Read(), p[1]) ? "YES" : "NO");
+            }
+        }
+        
+        public static bool Solve(int[] numbers, int k)
+        {
+            var positions = Enumerable.Range(1, numbers.Length).ToArray();
+            Array.Sort(numbers, positions);
+            var minSegments = 1;
+            
+            for (var i = 1; i < positions.Length; i++)
+            {
+                if (positions[i-1] + 1 != positions[i]) minSegments++;
+            }
+
+            return minSegments <= k && k <= numbers.Length;
+        }
+    }
+}
--- TaskTests/Contest1557Tests.cs
+++ TaskTests/Contest1557Tests.cs
@@ -13,14 +13,13 @@ namespace TaskTests
             Assert.True(TaskA.Solve(new long[] { 2, 2, 2 })- 4 < 0.0000001);
             Assert.True(TaskA.Solve(new long[] { 17, 3, 5, -3 })- 18.666666667 < 0.0000001);
         }
-        /*
-3, 1, 2
 
-4.500000000
--12.500000000
-4.000000000
-18.666666667
-
-         */
+        [Fact]
+        public void TaskBTest()
+        {
+            Assert.True(TaskB.Solve(new []{6, 3, 4, 2, 1}, 4));
+            Assert.False(TaskB.Solve(new []{1, -4, 0, -2}, 2));
+            Assert.True(TaskB.Solve(new []{1, 2, 3, 4, 5}, 1));
+        }
     }
 }
