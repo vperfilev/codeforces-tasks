9392f161ad5ba90630fb56a5a29aac63d29b7982
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-08-12 12:23:25 +0300

    Add 1555A

---
+++ CodeforcesTasks/Contest1555/TaskA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodeforcesTasks.Contest1555
+{
+    public class TaskA
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var number = long.Parse(Console.ReadLine()!);
+                Console.WriteLine(Solve(number));
+            }
+        }
+        public static long Solve(long count)
+        {
+            long pieces = 0;
+            if (count % 2 == 1) count++;
+            
+            if (count > 12)
+            {
+                pieces = count - 6 - count % 6;
+                count -= pieces;
+            }
+
+            if (count < 6) count = 6;
+            pieces += count;
+            return pieces / 2 * 5;
+        }
+    }
+}
---
+++ TaskTests/Content1555.cs
@@ -0,0 +1,35 @@
+﻿using CodeforcesTasks.Contest1555;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Content1555
+    {
+        [Fact]
+        public void TaskATest()
+        {
+            Assert.Equal(30, TaskA.Solve(12));
+            Assert.Equal(40, TaskA.Solve(15));
+            Assert.Equal(750, TaskA.Solve(300));
+            Assert.Equal(15, TaskA.Solve(1));
+            Assert.Equal(25000000000000000, TaskA.Solve(9999999999999999));
+            Assert.Equal(15, TaskA.Solve(3));
+        }
+    }
+}/*
+6
+
+
+
+
+
+
+*/
+/*
+Assert.Equal(30, TaskA.Solve(12));
+Assert.Equal(40, TaskA.Solve(15));
+Assert.Equal(750, TaskA.Solve(300));
+Assert.Equal(15, TaskA.Solve(1));
+Assert.Equal(25000000000000000, TaskA.Solve(9999999999999999));
+Assert.Equal(15, TaskA.Solve(3));
+*/
