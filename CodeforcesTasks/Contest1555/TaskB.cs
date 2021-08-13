f80de0f19e86e5ed02cd2eb952cb9ef0501402b3
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-08-13 13:19:35 +0300

    Add 1555B

---
+++ CodeforcesTasks/Contest1555/TaskB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CodeforcesTasks.Contest1555
+{
+    public class TaskB
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+        
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < count; i++)
+            {
+                var a = Read();
+                var b = Read();
+                var c = Read();
+                Console.WriteLine(Solve((a[0], a[1]), (b[0], b[1]), (b[2], b[3]), (c[0], c[1])));
+            }
+        }
+        
+        public static int Solve((int w, int h) roomSize, (int x, int y) tableCorner1, (int x, int y) tableCorner2,
+            (int w, int h) tableBSize)
+        {
+            var tableASize = (w: tableCorner2.x - tableCorner1.x, h: tableCorner2.y - tableCorner1.y);
+            var wPossible = tableBSize.w + tableASize.w <= roomSize.w;
+            var hPossible = tableBSize.h + tableASize.h <= roomSize.h;
+            if (!wPossible && !hPossible) return -1;
+
+            var distance = int.MaxValue;
+            if (wPossible)
+            {
+                var biggestSize = Math.Max(tableCorner1.x, roomSize.w - tableCorner2.x);
+                distance = tableBSize.w - biggestSize;
+            }
+
+            if (hPossible)
+            {
+                var biggestSize = Math.Max(tableCorner1.y, roomSize.h - tableCorner2.y);
+                distance = Math.Min(distance, tableBSize.h - biggestSize);
+            }
+
+            return distance < 0 ? 0 : distance;
+        }
+    }
+}
---
+++ TaskTests/Content1555Tests.cs
@@ -0,0 +1,37 @@
+﻿using CodeforcesTasks.Contest1555;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Content1555Tests
+    {
+        [Fact]
+        public void TaskATest()
+        {
+            Assert.Equal(30, TaskA.Solve(12));
+            Assert.Equal(40, TaskA.Solve(15));
+            Assert.Equal(750, TaskA.Solve(300));
+            Assert.Equal(15, TaskA.Solve(1));
+            Assert.Equal(25000000000000000, TaskA.Solve(9999999999999999));
+            Assert.Equal(15, TaskA.Solve(3));
+        }
+
+        [Fact]
+        public void TaskBTest()
+        {
+            Assert.Equal(1, TaskB.Solve((8, 5), (2, 1), (7, 4), (4, 2)));            
+            Assert.Equal(-1, TaskB.Solve((5, 4), (2, 2), (5, 4), (3, 3)));            
+            Assert.Equal(2, TaskB.Solve((1, 8), (0, 3), (1, 6), (1, 5)));            
+            Assert.Equal(2, TaskB.Solve((8, 1), (3, 0), (6, 1), (5, 1)));            
+            Assert.Equal(0, TaskB.Solve((8, 10), (4, 5), (7, 8), (8, 5)));
+            Assert.Equal(1, TaskB.Solve((1, 2), (0, 0), (1, 1), (1, 1)));
+        }
+        
+        /*
+,
+
+         *
+         * 
+         */
+    }
+}
