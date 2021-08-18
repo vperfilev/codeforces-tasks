288dd3f58ad8a15ef28faea8f15f7b63cb5c92b7
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-08-18 11:59:03 +0300

    Add 1512A

---
+++ CodeforcesTasks/Contest1512/TaskA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1512
+{
+    public class TaskA
+    {
+        static int[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < n; i++)
+            {
+                Console.ReadLine();
+                var data = Read();
+                var multiple = data[0];
+                if (data[0] != data[1]) multiple = data[0] == data[2] ? data[0] : data[1];
+                for (int j = 0; j < data.Length; j++)
+                {
+                    if (data[j]==multiple) continue;
+                    Console.WriteLine(j + 1);
+                    break;
+                }
+            }
+        }
+
+    }
+}
