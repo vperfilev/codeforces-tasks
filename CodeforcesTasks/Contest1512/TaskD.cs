cd0a1bdd982113b4677a2219fb94b95ea3281138
Vlad Perfilev <perfilev.vlad@yandex.ru>
2021-08-23 13:44:48 +0300

    Add 1512D

---
+++ CodeforcesTasks/Contest1512/TaskD.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeforcesTasks.Contest1512
+{
+    public class TaskD
+    {
+        static long[] Read() => Array.ConvertAll(Console.ReadLine()!.Split(), long.Parse);
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine()!);
+            for (var i = 0; i < n; i++)
+            {
+                Console.ReadLine();
+                Console.WriteLine(string.Join(' ', Solve(Read())));
+            }
+        }
+
+        public static long[] Solve(long[] bArray)
+        {
+            var bSum = bArray.Sum();
+            Array.Sort(bArray);
+            
+            for (var i = bArray.Length - 2; i < bArray.Length; i++)
+            {
+                var b = bArray[i];
+                var x = bSum - 2 * b;
+                if (x < 0) continue;
+                var xIndex = Array.BinarySearch(bArray, x);
+                var bIndex = Array.BinarySearch(bArray, b);
+                if (xIndex < 0 || bIndex < 0) continue;
+                if (bIndex == xIndex && bArray.Count(x => x ==b) < 2) continue;
+                        
+                var itemsToRemove = new List<long>(new[] { b, x });
+                var aArray = new long[bArray.Length - 2];
+                var aIndex = 0;
+                
+                foreach (var bValue in bArray)
+                {
+                    if (itemsToRemove.Contains(bValue))
+                        itemsToRemove.Remove(bValue);
+                    else
+                        aArray[aIndex++] = bValue;
+                }
+                return aArray;
+            }
+
+            return new long[]{-1};
+        }
+    }
+}
---
+++ TaskTests/Contest1512Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeforcesTasks.Contest1512;
+using Xunit;
+
+namespace TaskTests
+{
+    public class Contest1512Tests
+    {
+        [Fact]
+        public void TaskCTest()
+        {
+            var b1 = new long[] { 2, 3, 7, 12, 2 };
+            Assert.True(CheckCorrectionC(b1,TaskD.Solve(b1)));
+            
+            var b2 = new long[] { 9, 1, 7, 1, 6, 5 };
+            Assert.True(TaskD.Solve(b2).Single() == -1);
+            
+            var b3 = new long[] { 18, 2, 2, 3, 2, 9, 2 };
+            Assert.True(CheckCorrectionC(b3,TaskD.Solve(b3)));
+            
+            var b4 = new long[] { 2, 6, 9, 2, 1 };
+            Assert.True(CheckCorrectionC(b4,TaskD.Solve(b4)));
+
+            var b5 = new long[] { 1, 1, 1, 1, 2 };
+            Assert.True(TaskD.Solve(b5).Single() == -1);
+        }
+
+        private bool CheckCorrectionC(long[] b, long[] a)
+        {
+            var diff = new List<long>(2);
+            
+            Array.Sort(a);
+            Array.Sort(b);
+            var aPointer = 0;
+            var bPointer = 0;
+
+            while (aPointer < a.Length && bPointer < b.Length)
+            {
+                if (a[aPointer] != b[bPointer])
+                {
+                    diff.Add(b[bPointer]);
+                    bPointer++;
+                    continue;
+                }
+
+                aPointer++;
+                bPointer++;
+            }
+
+            while (bPointer < b.Length)
+            {
+                diff.Add(b[bPointer++]);
+            }
+
+            if (diff.Count != 2) return false;
+            var aSum = a.Sum();
+            return aSum == diff[0] || aSum == diff[1];
+        }
+    }
+}
