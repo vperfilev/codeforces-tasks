52755eb2e5214b4b2e059743f699cc1de36e0319
vperfilev <perfilev.vlad@yandex.ru>
2021-11-30 23:44:57 +0300

    Add remove node task

---
+++ CodeforcesTasks/Tasks/NodeRemoveTask.cs
@@ -0,0 +1,40 @@
+﻿namespace CodeforcesTasks.Tasks
+{
+    public static class NodeRemoveTask
+    {
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node Next { get; set; }
+            public override string ToString() => $"{Value} {Next}";
+        }
+
+        public static Node Remove(Node list, int number, out bool result)
+        {
+            result = false;
+            if (number < 1) return list;
+            
+            var rightPointer = list;
+            var leftPointer = list;
+            
+            while (number-- > 0)
+            {
+                if (rightPointer == null) return list;
+                rightPointer = rightPointer.Next;
+            }
+
+            result = true;
+            if (rightPointer == null) return list.Next;
+
+            while (rightPointer.Next != null)
+            {
+                rightPointer = rightPointer.Next;
+                leftPointer = leftPointer.Next;
+            } 
+
+            leftPointer.Next = leftPointer.Next.Next;
+            
+            return list;
+        }
+    }
+}
---
+++ TaskTests/NodeRemoveTaskTest.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using CodeforcesTasks.Tasks;
+using Xunit;
+
+namespace TaskTests
+{
+    public class NodeRemoveTaskTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var list = CreateNodeList(1, 2, 3, 4, 5);
+
+            var result =  NodeRemoveTask.Remove(list, 3, out _);
+            
+            Assert.Equal("1 2 4 5 ", result.ToString());
+        }
+        
+        [Fact]
+        public void Test2()
+        {
+            var list = CreateNodeList(1, 2, 3);
+
+            var result =  NodeRemoveTask.Remove(list, 1, out _);
+            
+            Assert.Equal("1 2 ", result.ToString());
+        }
+        
+        [Fact]
+        public void Test3()
+        {
+            var list = CreateNodeList(1, 2, 3);
+
+            var result =  NodeRemoveTask.Remove(list, 3, out _);
+            
+            Assert.Equal("2 3 ", result.ToString());
+        }
+        
+        [Fact]
+        public void Test4()
+        {
+            var list = CreateNodeList(1, 2, 3, 4, 5);
+
+            var result =  NodeRemoveTask.Remove(list, 1, out _);
+            
+            Assert.Equal("1 2 3 4 ", result.ToString());
+        }
+        
+        [Fact]
+        public void Test5()
+        {
+            var list = CreateNodeList(1, 2, 3, 4, 5);
+
+            var result =  NodeRemoveTask.Remove(list, 0, out _);
+            
+            Assert.Equal("1 2 3 4 5 ", result.ToString());
+        }
+        
+        [Fact]
+        public void Test6()
+        {
+            var list = CreateNodeList(1, 2, 3, 4, 5);
+
+            var result =  NodeRemoveTask.Remove(list, 6, out _);
+            
+            Assert.Equal("1 2 3 4 5 ", result.ToString());
+        }
+
+        private NodeRemoveTask.Node CreateNodeList(params int[] values)
+        {
+            NodeRemoveTask.Node node = null; 
+            foreach (var value in values.Reverse())
+            {
+                node = new NodeRemoveTask.Node {Value = value, Next = node};
+            }
+
+            return node;
+        }
+    }
+}
